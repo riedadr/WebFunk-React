96af6da8c43ab9401e4c7b4d7b91c5114c2894e3
karsten <riedadr@gmail.com>
2022-08-16 13:41:14 +0200

    before socket.io

--- src/components/Channels/kfdList.ts
+++ src/components/Channels/kfdList.ts
@@ -3,84 +3,84 @@ export const kfdList = [
 		name: "KFD 1",
 		Id: "kfd-1",
 		type: "kfd",
-		tcId: "tc-kfd-1", //tc: text chat
-		vcId: "vc-kfd-1", //vc: voice chat
+		tcId: "tc-kfd-1", 
+		vcId: "vc-kfd-1", 
 	},
 	{
 		name: "KFD 2",
 		Id: "kfd-2",
 		type: "kfd",
-		tcId: "tc-kfd-2", //tc: text chat
-		vcId: "vc-kfd-2", //vc: voice chat
+		tcId: "tc-kfd-2", 
+		vcId: "vc-kfd-2", 
 	},
 	{
 		name: "KFD 3",
 		Id: "kfd-3",
 		type: "kfd",
-		tcId: "tc-kfd-3", //tc: text chat
-		vcId: "vc-kfd-3", //vc: voice chat
+		tcId: "tc-kfd-3", 
+		vcId: "vc-kfd-3", 
 	},
 	{
 		name: "KFD 4",
 		Id: "kfd-4",
 		type: "kfd",
-		tcId: "tc-kfd-4", //tc: text chat
-		vcId: "vc-kfd-4", //vc: voice chat
+		tcId: "tc-kfd-4", 
+		vcId: "vc-kfd-4", 
 	},
 	{
 		name: "KFD 5",
 		Id: "kfd-5",
 		type: "kfd",
-		tcId: "tc-kfd-5", //tc: text chat
-		vcId: "vc-kfd-5", //vc: voice chat
+		tcId: "tc-kfd-5", 
+		vcId: "vc-kfd-5", 
 	},
 	{
 		name: "KFD 6",
 		Id: "kfd-6",
 		type: "kfd",
-		tcId: "tc-kfd-6", //tc: text chat
-		vcId: "vc-kfd-6", //vc: voice chat
+		tcId: "tc-kfd-6", 
+		vcId: "vc-kfd-6", 
 	},
 	{
 		name: "KFD 7",
 		Id: "kfd-7",
 		type: "kfd",
-		tcId: "tc-kfd-7", //tc: text chat
-		vcId: "vc-kfd-7", //vc: voice chat
+		tcId: "tc-kfd-7", 
+		vcId: "vc-kfd-7", 
 	},
 	{
 		name: "KFD 8",
 		Id: "kfd-8",
 		type: "kfd",
-		tcId: "tc-kfd-8", //tc: text chat
-		vcId: "vc-kfd-8", //vc: voice chat
+		tcId: "tc-kfd-8", 
+		vcId: "vc-kfd-8", 
 	},
 	{
 		name: "KFD 9",
 		Id: "kfd-9",
 		type: "kfd",
-		tcId: "tc-kfd-9", //tc: text chat
-		vcId: "vc-kfd-9", //vc: voice chat
+		tcId: "tc-kfd-9", 
+		vcId: "vc-kfd-9", 
 	},
 	{
 		name: "KFD 10",
 		Id: "kfd-10",
 		type: "kfd",
-		tcId: "tc-kfd-10", //tc: text chat
-		vcId: "vc-kfd-10", //vc: voice chat
+		tcId: "tc-kfd-10", 
+		vcId: "vc-kfd-10", 
 	},
 	{
 		name: "KFD 11",
 		Id: "kfd-11",
 		type: "kfd",
-		tcId: "tc-kfd-11", //tc: text chat
-		vcId: "vc-kfd-11", //vc: voice chat
+		tcId: "tc-kfd-11", 
+		vcId: "vc-kfd-11", 
 	},
 	{
 		name: "KFD 12",
 		Id: "kfd-12",
 		type: "kfd",
-		tcId: "tc-kfd-12", //tc: text chat
-		vcId: "vc-kfd-12", //vc: voice chat
+		tcId: "tc-kfd-12", 
+		vcId: "vc-kfd-12", 
 	},
 ];
